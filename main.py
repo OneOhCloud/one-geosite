bfa1d64b7d66386f789c4be28ed7d61813fe29e3
OneOhCloud <github@oneoh.cloud>
2025-04-24 12:29:55 +0800

    commit

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+"""Sing Box Rule Set Decompiler and Merger
+1. 下载 Sing Box
+2. 下载 geosite-cn.srs
+3. 执行 Sing Box 的 rule-set decompile 命令
+4. 合并规则
+5. 执行 Sing Box 的 rule-set compile 命令
+"""
+
+import json
 import os
 import platform
+import shutil
+import subprocess
+import tarfile
+
+import requests
 
 
 def get_system_info():
+    """
+    获取系统信息
+    :return: (os_platform, os_arch)
+    """
     # GitHub Actions 环境检测
     if os.getenv("GITHUB_ACTIONS"):
         return "linux", "amd64"  # GitHub hosted runner 使用 x64 架构
@@ -20,33 +38,36 @@ def get_system_info():
 
     platform_map = {"linux": "linux", "darwin": "darwin"}
 
-    os_arch = arch_map.get(machine)
-    if not os_arch:
+    system_arch = arch_map.get(machine)
+    if not system_arch:
         raise ValueError(f"不支持的系统架构: {machine}")
 
-    os_platform = platform_map.get(system)
+    detected_platform = platform_map.get(system)
     if not os_platform:
         raise ValueError(f"不支持的操作系统: {system}")
 
-    return os_platform, os_arch
+    return detected_platform, system_arch
 
 
 os_platform, os_arch = get_system_info()
 
-sing_box_ver = "1.11.7"
-
+# pylint: disable=invalid-name,C0301
+sing_box_ver = "1.11.8"
+# pylint: disable=invalid-name
 sing_box_url = f"https://github.com/SagerNet/sing-box/releases/download/v{sing_box_ver}/sing-box-{sing_box_ver}-{os_platform}-{os_arch}.tar.gz"
 sing_box_bin = "tmp/sing-box"
+# pylint: disable=invalid-name
 geosite_cn_url = "https://raw.githubusercontent.com/SagerNet/sing-geosite/refs/heads/rule-set/geosite-cn.srs"
 
 
 def download_sing_box():
-    import requests
-    import tarfile
+    """
+    下载 Sing Box
+    """
 
     os.makedirs("tmp", exist_ok=True)
 
-    def tar_filter(member, path):
+    def tar_filter(member, _):
         # 只允许 sing-box 可执行文件
         if member.name.endswith("sing-box"):
             member.name = os.path.basename(member.name)
@@ -56,7 +77,7 @@ def download_sing_box():
     if not os.path.exists(sing_box_bin):
         print("Downloading Sing Box...")
         print(f"URL: {sing_box_url}")
-        response = requests.get(sing_box_url)
+        response = requests.get(sing_box_url, timeout=10)
         with open("tmp/sing-box.tar.gz", "wb") as f:
             f.write(response.content)
         print("Download complete.")
@@ -71,14 +92,16 @@ def download_sing_box():
 
 # 下载 geosite-cn
 def download_geosite_cn():
-    import requests
+    """
+    下载 geosite-cn.srs
+    """
 
     os.makedirs("tmp", exist_ok=True)
 
     if not os.path.exists("tmp/geosite-cn.srs"):
         print("Downloading geosite-cn...")
         print(f"URL: {geosite_cn_url}")
-        response = requests.get(geosite_cn_url)
+        response = requests.get(geosite_cn_url, timeout=10)
         with open("tmp/geosite-cn.srs", "wb") as f:
             f.write(response.content)
         print("Download complete.")
@@ -90,8 +113,11 @@ def download_geosite_cn():
 
 
 def execute_sing_box(args="rule-set decompile tmp/geosite-cn.srs"):
-    import subprocess
-
+    """
+    执行 Sing Box
+    :param args: 命令行参数
+    :return:
+    """
     if not os.path.exists("tmp/geosite-cn.srs"):
         print("geosite-cn.srs not found. Please download it first.")
         return
@@ -100,24 +126,24 @@ def execute_sing_box(args="rule-set decompile tmp/geosite-cn.srs"):
     try:
         cmd = [sing_box_bin] + args.split()
         print(f"Command: {cmd}")
-        subprocess.run(
-           cmd , check=True
-        )
+        subprocess.run(cmd, check=True)
         print("Execution complete.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing Sing Box: {e}")
 
 
 def merge_rules():
-    import json
+    """
+    合并规则
+    """
 
     # 读取自定义规则
-    with open("rules.json", "r") as f:
+    with open("rules.json", "r", encoding="utf-8") as f:
         custom_rules = json.load(f)
 
     # 读取解压后的 geosite-cn.json
     try:
-        with open("tmp/geosite-cn.json", "r") as f:
+        with open("tmp/geosite-cn.json", "r", encoding="utf-8") as f:
 
             data = json.load(f)
             geosite_rules = data["rules"][0]
@@ -127,7 +153,7 @@ def merge_rules():
         )
         return
 
-    rule_keys = ["domain_regex", "domain", "domain_suffix"]
+    rule_keys = ["domain_regex", "domain", "domain_suffix", "ip_cidr"]
     print(geosite_rules.keys())
 
     for key in rule_keys:
@@ -147,19 +173,19 @@ def merge_rules():
     data["rules"] = [geosite_rules]
 
     # 保存更新后的文件
-    with open("tmp/geosite-one-cn.json", "w") as f:
+    with open("tmp/geosite-one-cn.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
 
     print("Rules merged successfully.")
 
-    execute_sing_box(
-        "rule-set compile tmp/geosite-one-cn.json"
-    )
+    execute_sing_box("rule-set compile tmp/geosite-one-cn.json")
+
 
 # 将 tmp/*.srs 移动到 output 目录
 def move_files():
-    import shutil
-
+    """
+    将 tmp/*.srs 移动到 output 目录
+    """
     os.makedirs("output", exist_ok=True)
 
     for file in os.listdir("tmp"):
@@ -175,4 +201,3 @@ if __name__ == "__main__":
     execute_sing_box()
     merge_rules()
     move_files()
-
